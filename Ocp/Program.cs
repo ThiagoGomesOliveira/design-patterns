709a9c8ffe7bee3da9f71ec6e586afa99b05017b
thiago gomes oliveira <thiago.blu1988@gmail.com>
2024-03-22 14:55:22 -0300

    Criação da classe vehicle para estudos do modelo OCP - Open/Closed Principle

---
+++ Ocp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ocp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TypeVehicle type = TypeVehicle.MOTORCYCLE;
+
+            if (type == TypeVehicle.CAR)
+            {
+                Vehicle vehicle = new Vehicle("Azul", 2024, 2.0, 5, 4);
+                vehicle.Car();
+            }
+            else
+            {
+                Vehicle vehicle = new Vehicle("Branco", 2024, 150, 0, 0);
+                vehicle.Motorcycle();
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
