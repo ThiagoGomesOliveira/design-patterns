27b8e9b04738567d33dea372d64d3c62affd8d6c
thiago gomes oliveira <thiago.blu1988@gmail.com>
2024-03-22 15:48:00 -0300

    Criação das classes para estudo do Liskov Substitution Principle - LSP

---
+++ Lsp/Payments/CreditCard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lsp.Payments
+{
+    class CreditCard : NubankCard
+    {
+        public override void Validate()
+        {
+            Console.WriteLine("Validando o limite ...");
+            Console.WriteLine("Limite OK!");
+        }
+    }
+}
---
+++ Lsp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Lsp.Payments;
+using System;
+
+namespace Lsp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //CreditCard card = new CreditCard();
+            DebitCard card = new DebitCard();
+            card.Validate();
+            card.CollectPayment();
+
+
+            Console.ReadLine(); 
+        }
+    }
+}
